86b0fbf010576c86a2fac7285185b6da9c7dcd6e
marvinbiss <marvin.bissohong@yeoskin.com>
2026-02-04 10:30:10 +0100

    Update loading.tsx

--- src/app/recherche/loading.tsx
+++ src/app/recherche/loading.tsx
@@ -1,4 +1,4 @@
-import { ArtisanCardSkeleton } from '@/components/ui/Skeleton'
+import { ProviderCardSkeleton } from '@/components/ui/Skeleton'
 
 export default function RechercheLoading() {
   return (
@@ -6,7 +6,7 @@ export default function RechercheLoading() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {[1, 2, 3, 4, 5, 6].map((i) => (
-            <ArtisanCardSkeleton key={i} />
+           <ProviderCardSkeleton key={i} />
           ))}
         </div>
       </div>
